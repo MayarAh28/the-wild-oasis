ea0d1f87910f0d33942e147f6fc27ba6558c8f80
Mayar <mayarah396@gmail.com>
2025-12-21 15:20:45 +0300

    Initial commit

---
+++ run_curl.cmd
@@ -0,0 +1,3 @@
+curl 'https://xwwiyujisitpkjtmmcwh.supabase.co/rest/v1/cabins?select=*' \
+-H "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3d2l5dWppc2l0cGtqdG1tY3doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNjAzMjMsImV4cCI6MjA3OTYzNjMyM30.ma-4vNiwGZFyC9fuTsd5WICQjOfqDLe-tCtmicn4xjQ" \
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3d2l5dWppc2l0cGtqdG1tY3doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNjAzMjMsImV4cCI6MjA3OTYzNjMyM30.ma-4vNiwGZFyC9fuTsd5WICQjOfqDLe-tCtmicn4xjQ"
